d0d9aab6453da6c8b100e33c4cda192c86befb62
wslominski <slominski@g.cofc.edu>
2023-07-19 11:57:34 -0400

    'Initial Commit'

---
+++ App.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const isAndroid = Platform.OS === 'android';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <>
+      <SafeAreaView style={{flex: 1, marginTop: StatusBar.currentHeight}}>
+        <View style={{padding: 16, backgroundColor: 'green'}}>
+          <Text> search </Text>
+        </View>
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: 'blue',
+            flex: 1,
+          }}>
+          <Text> List </Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
