c683f35afe6cb11b4598333529033e39e55e32fc
wslominski <slominski@g.cofc.edu>
2023-07-22 13:13:53 -0400

    Friday July 21st Commit

--- src/features/restaurants/screens/restaurants.screen.tsx
+++ src/features/restaurants/screens/restaurants.screen.tsx
@@ -10,22 +10,18 @@ import {
 import {Searchbar} from 'react-native-paper';
 import {RestaurantInfoCard} from '../../components/restuarants-info-card.component';
 import {Spacer} from '../../../components/spacer/spacer.component';
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
+import {SafeArea} from '../../../components/utils/safe-area.component';
 
 const SearchConatiner = styled(View)`
   padding: 16px;
-  background-color: darkorange;
+  background-color: white;
 `;
 
-const RestaurantListContainer = styled(View)`
-  padding: 16px;
-  background-color: blue;
-  flex: 1;
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export default function RestaurantsScreen() {
   const [searchQuery, setSearchQuery] = React.useState('');
@@ -43,7 +39,7 @@ export default function RestaurantsScreen() {
           value={searchQuery}
         />
       </SearchConatiner>
-      <FlatList
+      <RestaurantList
         data={[{name: 1}, {name: 2}, {name: 3}, {name: 4}]}
         renderItem={() => (
           <Spacer position="bottom" size="large">
@@ -51,7 +47,6 @@ export default function RestaurantsScreen() {
           </Spacer>
         )}
         keyExtractor={item => item.name}
-        contentContainerStyle={{padding: 16}}
       />
     </SafeArea>
   );
